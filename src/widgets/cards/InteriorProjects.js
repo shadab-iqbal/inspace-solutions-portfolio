f6cb44d4e7baf9b117a5d970d6859dc670fca1e1
Taher Abdullah <taherabdullah@Tahers-MacBook-Air.local>
2023-03-28 21:37:53 +0600

    contact page update

--- src/widgets/cards/InteriorProjects.js
+++ src/widgets/cards/InteriorProjects.js
@@ -1,61 +1,211 @@
 const InteriorProjects = [
   {
     id: "1",
-    title: "Amin Mohammmad Properties",
-    location: "Dhanmondi",
+    title: "Atul Khan Jute Mills Ltd",
+    location: "Sonargaon Road, Dhaka-1205",
     type: "interior",
     image: [
       {
         original:
-          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (1).jpg",
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (1).jpg",
         thumbnail:
-          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (1).jpg",
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (1).jpg",
       },
       {
         original:
-          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (2).jpg",
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (2).jpg",
         thumbnail:
-          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (2).jpg",
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (2).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/CONFERENCE (1).jpg",
+        thumbnail:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/CONFERENCE (1).jpg",
+      },
+      {
+        original:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/MEETING ROOM (2).jpg",
+        thumbnail:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/MEETING ROOM (2).jpg",
+      },
+      {
+        original:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/MEETING ROOM (2).jpg",
+        thumbnail:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/MEETING ROOM (2).jpg",
+      },
+      {
+        original:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (3).jpg",
+        thumbnail:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (3).jpg",
+      },
+      {
+        original:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (4).jpg",
+        thumbnail:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (4).jpg",
+      },
+      {
+        original:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (5).jpg",
+        thumbnail:
+        "../../../public/img/interior/Interior Office project/Altu Khan Jute Mills Limited (AKJML)- CONFERRENCE _ MEETING ROOM/RECEPTION _ CORIDORE AREA (5).jpg",
       },
     ],
   },
   {
     id: "2",
-    title: "Celio Bangladesh",
-    location: "Dhanmondi",
+    title: "Amin Mohammad Properties",
+    location: "Dhanmondi, Dhaka",
     type: "interior",
     image: [
       {
         original:
-          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/1.jpg",
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (8).jpg",
         thumbnail:
-          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/1.jpg",
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (8).jpg",
       },
       {
         original:
-          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/2.jpg",
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (7).jpg",
         thumbnail:
-          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/2.jpg",
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (7).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (6).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (6).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (5).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (5).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (4).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (4).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (3).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (3).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (2).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (2).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (1).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Reception (1).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Meeting Room (2).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Meeting Room (2).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Meeting Room (1).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Meeting Room (1).jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Graphics (2).jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Amin Mohammmad Properties- Renovation of Office Interior- Dhanmondi, Dhaka/Graphics (2).jpg",
       },
     ],
   },
   {
     id: "3",
-    title: "DHL",
-    location: "Bashundhara",
+    title: "Celio Bangladesh",
+    location: "Gulshan-02, Dhaka",
     type: "interior",
     image: [
       {
         original:
-          "../../../public/img/interior/Interior Office project/DHL Conference room- Tejgoan, Dhaka/Open hollroom (1).jpg",
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/10.jpg",
         thumbnail:
-          "../../../public/img/interior/Interior Office project/DHL Conference room- Tejgoan, Dhaka/Open hollroom (1).jpg",
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/10.jpg",
       },
       {
         original:
-          "../../../public/img/interior/Interior Office project/DHL Conference room- Tejgoan, Dhaka/Open hollroom (2).jpg",
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/6.jpg",
         thumbnail:
-          "../../../public/img/interior/Interior Office project/DHL Conference room- Tejgoan, Dhaka/Open hollroom (2).jpg",
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/6.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/8.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/8.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/9.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/9.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/11.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/11.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/12.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/12.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/13.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/13.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/15.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/15.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/16.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/16.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/19.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/19.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/2.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/2.jpg",
+      },
+      {
+        original:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/20.jpg",
+        thumbnail:
+          "../../../public/img/interior/Interior Office project/Celio Bangladesh- Office Interior- Gulshan-02, Dhaka/20.jpg",
       },
     ],
   },
